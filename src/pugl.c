d2fba183dc2bc79ac9a4c5dc113defa24cd1671e
osch <oliver at luced de>
2021-02-17 18:52:02 +0100

    removed unnecessary include

--- src/pugl.c
+++ src/pugl.c
@@ -4,14 +4,12 @@
 
 #if defined(LPUGL_USE_WIN)
     #include "pugl-repo/src/win.c"
-    #include "pugl-repo/src/win_stub.c"
 
 #elif defined(LPUGL_USE_MAC)
     #error use pugl.m for mac
 
 #elif defined(LPUGL_USE_X11)
     #include "pugl-repo/src/x11.c"
-    #include "pugl-repo/src/x11_stub.c"
 
 #else
     #error missing platform definition
