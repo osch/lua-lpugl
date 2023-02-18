3aec0a1c27d6558fd2762d3fef9f4404a0a673a3
osch <oliver at luced de>
2023-02-18 13:13:06 +0100

    activate script improved

--- src/activate.sh
+++ src/activate.sh
@@ -52,13 +52,22 @@ else
     add_lua_path="$lualpugl_dir/src/?.lua:$lualpugl_dir/src/?/init.lua"
     add_lua_cpath="$lualpugl_dir/src/build"
 
-    # unset LUA_PATH_5_3 LUA_CPATH_5_3 LUA_PATH_5_2 LUA_CPATH_5_2 LUA_PATH LUA_CPATH
+    # unset LUA_PATH_5_4 LUA_CPATH_5_4 LUA_PATH_5_3 LUA_CPATH_5_3 LUA_PATH_5_2 LUA_CPATH_5_2 LUA_PATH LUA_CPATH
 
     default_version=""
     if which lua > /dev/null 2>&1; then
         default_version=$(lua -e 'v=_VERSION:gsub("^Lua ","");print(v)')
     fi
     
+    if [ -n "$default_version" ]; then
+        if [ "$default_version" != "5.1" ]; then
+            echo "Setting path for lua (version=$default_version)"
+            lua_path_vers=$(echo $default_version|sed 's/\./_/')
+            eval "export LUA_PATH_$lua_path_vers=\"$(path_to_dos $add_lua_path:$(path_to_unix $(lua -e 'print(package.path)')))\""
+            eval "export LUA_CPATH_$lua_path_vers=\"$(path_to_dos $add_lua_cpath/lua$default_version/?.$so_ext:$(path_to_unix $(lua -e 'print(package.cpath)')))\""
+        fi
+    fi
+
     for vers in 5.4 5.3 5.2 5.1; do
         lua_cmd=""
         if which lua$vers > /dev/null 2>&1; then
@@ -83,14 +92,10 @@ else
     done
     
     if [ -n "$default_version" ]; then
-        echo "Setting path for lua (version=$default_version)"
         if [ "$default_version" = "5.1" ]; then
+            echo "Setting path for lua (version=$default_version)"
             export LUA_PATH="$(path_to_dos $add_lua_path:$(path_to_unix $(lua -e 'print(package.path)')))"
             export LUA_CPATH="$(path_to_dos $add_lua_cpath/lua5.1/?.$so_ext:$(path_to_unix $(lua -e 'print(package.cpath)')))"
-        else
-            lua_path_vers=$(echo $default_version|sed 's/\./_/')
-            eval "export LUA_PATH_$lua_path_vers=\"$(path_to_dos $add_lua_path:$(path_to_unix $(lua -e 'print(package.path)')))\""
-            eval "export LUA_CPATH_$lua_path_vers=\"$(path_to_dos $add_lua_cpath/lua$default_version/?.$so_ext:$(path_to_unix $(lua -e 'print(package.cpath)')))\""
         fi
     fi
 fi
